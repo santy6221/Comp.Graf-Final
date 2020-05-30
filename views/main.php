8eecbe00fd06acd5a50fe4edd88330cbab6312d5
Santiago <srt6221@gmail.com>
2020-05-29 19:11:44 -0500

    main

--- views/main.php
+++ views/main.php
@@ -1,2 +1,35 @@
-<php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../resources/bootstrap.min.css">
+    <script src="../resources/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../styles/main.css">
+    <title>Document</title>
+</head>
+
+<body style="background-color: #E4F1F2">
+
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3B4859">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#" aria-disabled="true">Protocolos <span class="sr-only">(Current)</span></a>
+                <a class="nav-item nav-link" href="#">Historial</a>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="boxed" style="background-color: #3B4859;">
+        <h3>alooooooooooo</h3>
+    </div>
+
+
+</body>
+
+</html>
